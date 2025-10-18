50f41b6f4f4ff7641577da77143ecc2a3eba398a
goodylove <goodyc474@gmail.com>
2025-10-18 10:34:01 +0100

    fix: resolve runtime bug and ensure server runs correctly

--- services/catFact.js
+++ services/catFact.js
@@ -5,14 +5,14 @@ import axios from "axios";
 export const fetchCatFact = async () => {
   let defaultFact = "A cat's appetite is the barometer of its health. Any cat that does not eat or drink for more than two days should be taken to a vet."
   try {
-    const {data} = await axios.get(process.env.CAT_API_URL, {
+    const { data } = await axios.get(process.env.CAT_API_URL, {
       headers: { "Cache-Control": "no-cache" },
 
       timeout: 5000,
     });
-    
-    
-   return  data.fact || defaultFact
+
+
+    return data.fact || defaultFact
   } catch (error) {
     console.log("Error Occurred While Fetching Cat Fact:", error.message);
     return defaultFact
